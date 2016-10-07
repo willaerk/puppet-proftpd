c9e2e1c1025faf7ed5ac85de7e749f63289290b1
Kristof Willaert <kristof.willaert@gmail.com>
2016-10-07 13:36:49 +0200

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,6 +3,7 @@
 class proftpd::service {
   if ( $::proftpd::service_manage == true ) {
     service { $::proftpd::service_name:
+      hasrestart => true,
       ensure  => $::proftpd::service_ensure,
       enable  => $::proftpd::service_enable,
       require => Class['::proftpd::config'],
